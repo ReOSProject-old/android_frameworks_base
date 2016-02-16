e645701c6142b6b0325fbbe4459ec4d2fd53f4f5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-02-16 18:30:06 +0100

    CellularTile is now a toggle
    Single tap to toggle on/off
    long press to open detailView
    add secondaryClick to open detailView
    
    Change-Id: I8db64db246ccc7150753565664eef3de8a90d584
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -41,8 +41,11 @@ import com.android.systemui.statusbar.policy.SignalCallbackAdapter;
 
 /** Quick settings tile: Cellular **/
 public class CellularTile extends QSTile<QSTile.SignalState> {
+
     private static final Intent CELLULAR_SETTINGS = new Intent().setComponent(new ComponentName(
             "com.android.settings", "com.android.settings.Settings$DataUsageSummaryActivity"));
+	
+	private static final Intent MOBILE_NETWORK_SETTINGS = new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS);
 
     private final NetworkController mController;
     private final MobileDataController mDataController;
@@ -90,11 +93,34 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
 
     @Override
     protected void handleClick() {
+        boolean enabled = mDataController.isMobileDataEnabled();
         MetricsLogger.action(mContext, getMetricsCategory());
+        if (mDataController.isMobileDataSupported()) {
+            if (!enabled) {
+                mDataController.setMobileDataEnabled(true);
+            } else {
+                mDataController.setMobileDataEnabled(false);
+            }
+        } else {
+            mHost.startActivityDismissingKeyguard(CELLULAR_SETTINGS);
+        }
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
         if (mDataController.isMobileDataSupported()) {
             showDetail(true);
         } else {
-            mHost.startActivityDismissingKeyguard(CELLULAR_SETTINGS);
+            mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
+        }
+    }
+
+    @Override
+    protected void handleLongClick() {
+        if (mDataController.isMobileDataSupported()) {
+            showDetail(true);
+        } else {
+            mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
         }
     }
 
